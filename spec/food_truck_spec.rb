f2b66dab3fa0732e059c1fb38eb5b6520096179f
Lucas Colwell <colwellc37@gmail.com>
2022-04-26 09:44:15 -0600

    Test: write tests for check/stock

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -4,6 +4,8 @@ require './lib/food_truck'
 RSpec.describe FoodTruck do
   before :each do
     @food_truck = FoodTruck.new("Rocky Mountain Pies")
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
   end
 
   it 'exists' do
@@ -13,5 +15,24 @@ RSpec.describe FoodTruck do
   it 'has attributes' do
     expect(@food_truck.name).to eq("Rocky Mountain Pies")
     expect(@food_truck.inventory).to eq({})
+  end
+
+  it 'can check stock' do
+    expect(@food_truck.check_stock(@item1)).to eq(0)
+  end
+
+  it 'can stock items / add to inventory' do
+    @food_truck.stock(@item1, 30)
+
+    expect(@food_truck.inventory).to eq({@item1 => 30})
+    expect(@food_truck.check_stock(@item1)).to eq(30)
+
+    @food_truck.stock(@item1, 25)
+
+    expect(@food_truck.check_stock(@item1)).to eq(55)
+
+    @food_truck.stock(@item2, 12)
+
+    expect(@ood_truck.inventory).to eq({@item1 => 55, @item2 => 12})
   end
 end
